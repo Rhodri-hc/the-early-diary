65d41482201acf42abfa7572b228f7dc5650edac
Rhodri-hc <13622450494@163.com>
2021-05-29 23:31:51 +0800

    节流

--- debounce&throttle/throttle.js
+++ debounce&throttle/throttle.js
@@ -5,7 +5,10 @@ function getUserAction() {
     container.innerHTML = count++;
 };
 
-container.onmousemove = throttle1(getUserAction, 1000, true);
+container.onmousemove = throttle3(getUserAction, 1000, {
+    // trailing: false
+    leading: false
+});
 
 
 /**
@@ -27,4 +30,73 @@ function throttle1(func, wait){
             },wait)
         }
     }
+}
+
+/**
+* @desc 节流第二版 
+* @author 张和潮
+* @date 2021年05月29日 13:21
+*/
+function throttle2(func, wait){
+    let context, args;
+    let pervious = 0;
+
+    return function(){
+        const now = +new Date()
+
+        context = this;
+        args = arguments;
+        
+        if(now - pervious > wait){
+            func.apply(context, args)
+            pervious = now;
+        }
+    }
+}
+
+/**
+* @desc 节流第三版
+* @author 张和潮
+* @date 2021年05月29日 21:16
+*/
+function throttle3(func, wait, options = {}) {
+    let context, args, timeout;
+    let previous = 0;
+    
+    const later = function(){
+        previous = options.leading === false ? 0 : new Date().getTime();
+        timeout = null;
+
+        func.apply(context, args);
+        if(!timeout){
+            context = args = null
+        }
+    }
+
+    const throttle = function(){
+        let now = new Date().getTime();
+
+        if(!previous && options.leading === false){
+            previous = now;
+        }
+
+        let remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+
+        if(remaining <= 0 || remaining > wait){
+            if(timeout){
+                clearTimeout(timeout);
+                timeout = null;
+            }
+            previous = now;
+            func.apply(context, args);
+            if(!timeout){
+                context = args = null;
+            }
+        }else if(!timeout && options.trailing !== false){
+            timeout = setTimeout(later, remaining);
+        }
+    };
+    return throttle;
 }
