1efbddace405d1136bc23d39fabce37f0b1873b6
Rhodri-hc <13622450494@163.com>
2022-07-21 16:18:36 +0800

    vue3 工具函数

--- sourceCode/type/index.js
+++ sourceCode/type/index.js
@@ -114,3 +114,113 @@ function isStandardBrowserEnv() {
       typeof document !== 'undefined'
     );
 }
+
+/**
+* @desc 移除数组
+* @author 张和潮
+* @date 2022年07月21日 15:18:23
+*/
+const remove = (arr, el) => {
+    const i = arr.indexOf(el);
+    if (i > -1) {
+        arr.splice(i, 1);
+    }
+};
+/**
+ * splice 其实是一个很耗性能的方法。删除数组中的一项，其他元素都要移动位置。
+ * 
+ * 引申：axios InterceptorManager 拦截器源码 中，拦截器用数组存储的。但实际移除拦截器时，只是把拦截器置为 null 。
+ * 而不是用splice移除。最后执行时为 null 的不执行，同样效果。axios 拦截器这个场景下，不得不说为性能做到了很好的考虑。
+ * 
+ */
+
+/**
+* @desc 是不是函数
+* @author 张和潮
+* @date 2022年07月21日 15:27:13
+*/
+const isFunction = (val) => typeof val === 'function';
+
+/**
+* @desc 判断是不是 Promise
+* @author 张和潮
+* @date 2022年07月21日 15:26:36
+*/
+const isPromise = (val) => {
+    return isObject(val) && isFunction(val.then) && isFunction(val.catch);
+};
+
+/**
+* @desc 判断是不是数字型的字符串key值
+* @author 张和潮
+* @date 2022年07月21日 15:34:13
+*/
+const isIntegerKey = (key) => isString(key) &&
+                                key !== 'NaN' &&
+                                key[0] !== '-' &&
+                                '' + parseInt(key, 10) === key;
+
+/**
+* @desc cacheStringFunction 缓存
+* @author 张和潮
+* @date 2022年07月21日 15:46:22
+*/
+const cacheStringFunction = (fn) => {
+    const cache = Object.create(null);
+    return ((str) => {
+        const hit = cache[str];
+        return hit || (cache[str] = fn(str));
+    });
+};
+/**
+* @desc 获取单例
+* @author 张和潮
+* @date 2022年07月21日 15:47:17
+*/
+var getSingle = function(fn){
+    var result;
+    return function(){
+        return result || (result = fn.apply(this, arguments));
+    }
+};
+
+
+/**
+* @desc 判断是不是有变化
+* @author 张和潮
+* @date 2022年07月21日 15:48:49
+*/
+const hasChanged = (value, oldValue) => !Object.is(value, oldValue);
+
+
+/**
+* @desc getGlobalThis 全局对象
+* @author 张和潮
+* @date 2022年07月21日 16:14:32
+*/
+let _globalThis;
+const getGlobalThis = () => {
+    return (_globalThis ||
+        (_globalThis =
+            typeof globalThis !== 'undefined'
+                ? globalThis
+                : typeof self !== 'undefined'
+                    ? self
+                    : typeof window !== 'undefined'
+                        ? window
+                        : typeof global !== 'undefined'
+                            ? global
+                            : {}));
+};
+
+/**
+ * 
+获取全局 this 指向。
+初次执行肯定是 _globalThis 是 undefined。所以会执行后面的赋值语句。
+如果存在 globalThis 就用 globalThis。MDN globalThis
+如果存在self，就用self。在 Web Worker 中不能访问到 window 对象，但是我们却能通过 self 访问到 Worker 环境中的全局对象。
+如果存在window，就用window。
+如果存在global，就用global。Node环境下，使用global。
+如果都不存在，使用空对象。可能是微信小程序环境下。
+ 
+ */
